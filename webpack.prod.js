18835dab1852a31b15e4cb9d648aaf430b77659e
Miral-Issa <issameral@gmail.com>
2025-02-28 17:03:14 +0200

    hopefully final branch

---
+++ webpack.prod.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin');
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new WorkboxPlugin.GenerateSW(),
+    ],
+    devServer: {
+        port: 3000,
+        allowedHosts: 'all',
+
+        static: 
+        {
+            directory: path.join(__dirname, 'dist'),
+        },
+        hot: true,
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'images/[name][ext]', 
+    }
+}
